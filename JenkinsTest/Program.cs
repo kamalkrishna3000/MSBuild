69ff30f7c95d737c951f383dfdb2afcfb480f124
Murali Krishna <murali.krishna@us.insigmainc.com>
2016-02-09 19:54:06 +0530

    Initial Version.

---
+++ JenkinsTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JenkinsTest
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine(" Jenkins executed this file");
+			Console.ReadKey();
+		}
+	}
+}
